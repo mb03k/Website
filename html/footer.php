ddc967fb2a08b657c528b67a76168a550b2298e4
matthes <matthesboettcher03@gmail.com>
2024-10-11 18:04:38 +0200

    footer fuer Websiten gebaut

---
+++ html/footer.php
@@ -0,0 +1,76 @@
+<?php echo('
+<br>
+<br>
+<br>
+<footer class="bg-dark text-center text-lg-start text-white">
+    <div class="container p-2">
+        <div class="row mt-4">
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Motivationssprüche</h5>
+                <ul class="list-unstyled mb-0">
+                    <li>
+                        Wow, du siehst heute fantastisch aus! Hast du die ganze Seite gelesen? Beeindruckend!
+                    </li>
+                    <br>
+                    <li>
+                        Ist dein Name genau so schön wie du?
+                    </li>
+                    <br>
+                    <li>
+                        Du bist die Art Besucher, von der ich geträumt habe.
+                    </li>
+                </ul>
+            </div>
+        
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Ein Hauch Philosophie</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        Wenn eine Webseite im Internet offline geht und niemand da ist, um es zu sehen, ist sie wirklich weg?
+                    </li>
+                    <br>
+                    <li>
+                        Okay ja das war es schon...
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Kontakt</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        Ich gebe dir sehr gerne die Möglichkeit mir eine Email zu schreiben.
+                        Leider weiß ich nicht wie das geht.
+                        Deshalb geht das noch nicht - aber es ist in arbeit, versprochen!
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Info</h5>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!" class="text-white"><i class="fas fa-at fa-fw fa-sm me-2"></i>FAQ</a>
+                    </li>
+
+                    <li>
+                        <a href="#!" class="text-white"><i class="fas fa-shipping-fast fa-fw fa-sm me-2"></i>Impressum</a>
+                    </li>
+
+                    <li>
+                        <a href="#!" class="text-white"><i class="fas fa-envelope fa-fw fa-sm me-2"></i>Datenschutz</a>
+                    </li>
+
+                    <li>
+                        <a href="#!" class="text-white"><i class="fas fa-envelope fa-fw fa-sm me-2"></i>AGB</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2)">
+    © Matthes B., alle Rechte und Links reserviert – auch die unsichtbaren
+    </div>
+</footer>
+');
