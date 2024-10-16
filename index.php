1e9c17d50c2c797820d315a6b69fdea1494d8b55
matthes <matthesboettcher03@gmail.com>
2024-10-16 08:25:16 +0200

    kleine Anpassungen

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meine Website</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
-    <link rel="stylesheet" href="Style/style.css">
-    <link rel="stylesheet" href="Style/homestyle.css">
-    <link rel="stylesheet" href="Style/header.css">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
-    <!--<link rel="stylesheet" href="Style/homeScreenAnimations.css"> bereitet in der Form Design-Fehler (out-of-bildschirm)-->
-</head>
+<?php include 'html/head.php'; ?>
 
 <body class="gradient">
     <div>
-        
         <?php include 'html/header.php'; ?>
 
         <div class="container">
@@ -37,7 +24,7 @@
 
             <br>
 
-            <div class="row text-dark text-center" style="display:flex;align-items: center; height:100%;">
+            <div class="row text-dark text-center d-flex align-items-center" style="height:100%;">
                 <div class="col-md m-2 projectsContainer">
                     <div class="col p-2">
                         <div class="rowOneCurrentProjects" id="currentProjects">
@@ -104,9 +91,11 @@
         <div class="container text-center">
             <p class="m-4">
                 <span>
-                    Es gibt sehr viel über mich. Ich habe unglaublich viele Ideen was ich hier
-                    alles 'veröffentlichen' kann (in Klammern, weil es realistisch gesehen niemanden interessieren
-                    wird). Wie wäre es mit - zugegebenermaßen - einer sehr schönen Auswahl an Filmen?
+                    Ich habe unglaublich viele Ideen darüber, was ich hier alles 'veröffentlichen' will. 
+                    Ich habe mich aber etwas zu sehr reingestürzt und den ganzen Aufwand unterschätzt,
+                    wie schwer es sein kann eine Seite überhaupt mit Text zu füllen. Deshalb lasse ich es
+                    klein angehen. Wie wäre es denn in der Zwischenzeit mit einer - zugegebenermaßen - 
+                    sehr schönen Auswahl an <a href="https://weristmatthes.de/popcornphilosoph">Filmen</a>?
                 </span>
             </p>
 
@@ -116,7 +105,7 @@
                     Oder schau dich sonst um. Hier hast du eine Liste aller aktuell verfügbaren Seiten
                 </span>
             </p>
-            <div style="display: flex; justify-content: center;">
+            <div class="mb-4" style="display: flex; justify-content: center;">
                 <div class="list-group" style="width: 50%;">
                     <button type="button" class="list-group-item list-group-item-action available_sites" id="aboutme_link">Über mich</button>
                     <button type="button" class="list-group-item list-group-item-action available_sites" id="popcornphilosoph_link">Filme / Bücher</button>
@@ -125,13 +114,6 @@
                     <button type="button" class="list-group-item list-group-item-action available_sites" id="faq_link">FAQ</button>
                 </div>
             </div>
-
-            <button class="btn btn-dark" id="404_link">404</button>
-            <button class="btn btn-dark" id="aboutme_link"aboutme>aboutme</button>
-            <button class="btn btn-dark" id="aberbitteohneinstagram_link">aboutme/aberbitteohneinstagram</button>
-            +<button class="btn btn-dark" id="faq_link">faq</button>
-            <button class="btn btn-dark" id="popcornphilosoph_link">popcornphilosoph</button>
-            <button class="btn btn-dark" id="projekte_link">projekte</button>
 
             <span>Willst du etwas <a href="#" id="readspeed_button">cooles</a> sehen?</span>
         </div>
