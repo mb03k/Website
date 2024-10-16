01961218d404e4a7778974099d8e95699b8b9dac
matthes <matthesboettcher03@gmail.com>
2024-10-16 08:27:31 +0200

    Idee eines Textes hinzugefuegt

--- projekte/p_website.php
+++ projekte/p_website.php
@@ -1,3 +1,29 @@
 <?php echo('
-    
+    <p>
+        Eine Website zu erstellen ist doch eigentlich ganz einfach. Und damit hast du recht. 
+        Das ist es!
+        Ich wollte aber nicht den 0815 Weg mit einer Wordpress-Website gehen. Im Internet sind 
+        unglaubliche 43% aller Seiten von CMS (Content-Management-Systems) erstellt. D.h. Wordpress
+        oder ähnliches. Und dazu habe ich sogar eine Quelle: <a href="https://www.ionos.de/digitalguide/hosting/cms/cms-vergleich/#:~:text=Der%20Analysewebsite%20W3Techs%20zufolge%20basieren,%2C%20sind%20Joomla!%20und%20Drupal.">
+        Ionos</a> schreibt das auf ihrer Website. Verlinken
+        im Endeffekt aber zu <a href="https://w3techs.com/technologies/overview/content_management">
+        dieser Seite</a>. Aber wer kann es einem auch übel nehmen; es ist um welten angenehmer als sich
+        alles selbst beizubringen. HTML, CSS, JavaScript, PHP, SQL, Angular, React, Next.js, Vue.js und 1000
+        andere Frameworks und Programmiersprachen, die man benutzen kann. Hier einen Überblick zu bekommen
+        ist wirklich schwer. 
+        Trotzdem habe ich seit langen den Traum einer eigenen Website. Und jetzt sitze ich hier.
+        Schreibe diesen Text. <b>Auf meiner EIGENEN Website</b>. Cool, oder nicht?
+        <br><br>
+        Aber naja. Hier mal einen kleinen Überblick in die Sachen die ich hierfür machen musste:
+        A) selbstverständlich das Wichtigste hierfür mieten. Die Domain. Das geht leicht über die
+        verschiedensten Anbieter
+        B) einen Server mieten. Das ist auch sehr leicht. Für mich läuft die Domain und der Server
+        über die <a href="https://www.strato.de/">Strato AG</a> und kostet 
+        <a href="https://www.weristmatthes.de/faq"> siehe hier</a> *schleichwerbung für mein FAQ*
+        C) hier fängt aber der eigentliche Schmerz an. Ich habe mir den günstigsten Server gemietet.
+        Einen "VPS Linux VC1-1" - einen virtuellen Server. Sich mit dem überhaupt erst zu verbinden 
+        war für mich von Anfang an das schwerste. Naja, nach viel herumprobieren hat das auch geklappt.
+        Dann habe ich im laufe der Tage sehr viel darüber gelernt, wie man mit einem Server kommuniziert,
+        den man nicht anfassen kann. 
+    </p>
 '); ?>
