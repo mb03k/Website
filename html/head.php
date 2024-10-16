a04faa72ecc99ae2120903ff4d815e1c47d26097
matthes <matthesboettcher03@gmail.com>
2024-10-16 08:28:27 +0200

    Vorgefertigte Head-Datei

---
+++ html/head.php
@@ -0,0 +1,15 @@
+<?php echo('
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matthes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
+    <link rel="stylesheet" href="Style/style.css">
+    <link rel="stylesheet" href="Style/homestyle.css">
+    <link rel="stylesheet" href="Style/header.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+    <link rel="icon" href="Assets/M.png">
+</head>
+'); ?>
