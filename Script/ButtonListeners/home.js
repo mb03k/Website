4e5e9469782b692166fb3c91e761a3c607708e1f
matthes <matthesboettcher03@gmail.com>
2024-10-20 00:05:52 +0200

    Verkuerzung der benoetigten Methoden

--- Script/ButtonListeners/home.js
+++ Script/ButtonListeners/home.js
@@ -1,39 +1,5 @@
-document.getElementById("readspeed_button").onclick = function() {
-    window.open("http://localhost/Website/readspeed", "_self");
-}
-
-document.getElementById("projectChess").onclick = function() {
-    window.open("http://localhost/Website/projekte", "_self");
-}
-
-document.getElementById("projectCheckers").onclick = function() {
-    window.open("http://localhost/Website/projekte", "_self");
-}
-
-document.getElementById("aboutme").onclick = function() {
-    window.open("http://localhost/Website/aboutme", "_self");
-}
-
-/* list with website links */
-
-document.getElementById("aboutme_link").onclick = function() {
-    window.open("https://weristmatthes.de/aboutme", "_self");
-}
-
-document.getElementById("faq_link").onclick = function() {
-    window.open("https://weristmatthes.de/faq", "_self");
-}
-
-document.getElementById("popcornphilosoph_link").onclick = function() {
-    window.open("https://weristmatthes.de/popcornphilosoph", "_self");
-}
-
-document.getElementById("projekte_link").onclick = function() {
-    window.open("https://weristmatthes.de/projekte", "_self");
-}
-
-document.getElementById("readspeed_link").onclick = function() {
-    window.open("https://weristmatthes.de/readspeed", "_self");
+document.getElementsByClassName("meinButton").onclick = function() {
+    console.log("GEKLICKT");
 }
 
 document.getElementById("bootstrap_button").onclick = function() {
