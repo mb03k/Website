dbe7d1cec86cac23ba7736f675acdee2a6ed499b
matthes <matthesboettcher03@gmail.com>
2024-10-20 00:08:01 +0200

    Styling angepasst

--- projekte/index.php
+++ projekte/index.php
@@ -3,18 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meine Website</title>
+    <title>Matthes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
     <link rel="stylesheet" href="../Style/style.css">
     <link rel="stylesheet" href="../Style/header.css">
+    <link rel="stylesheet" href="../Style/projects.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-    <!--<link rel="stylesheet" href="Style/homeScreenAnimations.css"> bereitet in der Form Design-Fehler (out-of-bildschirm)-->
 </head>
 
 <body class="gradient">
@@ -22,25 +21,43 @@
         <?php include '../html/header.php'; ?>
         
         <div class="container">
+            <div class="h1 text-center">
+                <span class="my_projects_headline">
+                    <p class="bg-danger rounded m-4">
+                        Meine Projekte
+                    </p>
+                </span>
+            </div>
 
             <div class="project_website">
                 <?php include 'p_website.php'; ?>
             </div>
 
+            <hr class="m-4" style="border-width: 0.5vh; width: 90%; margin:auto;">
+
             <div class="project_chess">
                 <?php include 'p_schach.php'; ?>
             </div>
+
+            <hr class="m-4" style="border-width: 0.5vh; width: 90%; margin:auto;">
 
             <div class="project_checkers">
                 <?php include 'p_dame.php'; ?>
             </div>
 
+            <hr class="m-4" style="border-width: 0.5vh; width: 90%; margin:auto;">
+
             <div class="project_calculator">
                 <?php include 'p_taschenrechner.php'; ?>
             </div>
+
+            <hr class="m-4" style="border-width: 0.5vh; width: 90%; margin:auto;">
+
+            <div class="projekt_multimengen">
+                <?php include 'p_multimengen.php'; ?>
+            </div>
         </div>
 
-        <?php include 'p_multimengen.php'; ?>
         
         <?php include '../html/footer.php'; ?>
 
